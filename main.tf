ff18ea84f2f8222ec5539987a40d9cd4d071d910
Chris Coussa <ccoussa717@gmail.com>
2022-07-26 20:57:12 -0500

    update to main.tf

--- main.tf
+++ main.tf
@@ -28,17 +28,17 @@ resource "azurerm_network_security_group" "security_group" {
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
 
-    security_rule {
-    name                       = "ssh"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
+  #   security_rule {
+  #   name                       = "ssh"
+  #   priority                   = 100
+  #   direction                  = "Inbound"
+  #   access                     = "Allow"
+  #   protocol                   = "Tcp"
+  #   source_port_range          = "*"
+  #   destination_port_range     = "22"
+  #   source_address_prefix      = "*"
+  #   destination_address_prefix = "*"
+  # }
 
   tags = {
     environment = "Development"
